24e33adeeb7e591dc8bf32890c6959662b7ad5c2
Keith M Lawrence <keith@kludge.co.uk>
2014-08-25 18:19:30 +0100

    Add tests for #check, and code to pass them

--- lib/unenviable.rb
+++ lib/unenviable.rb
@@ -2,7 +2,36 @@
 require 'yaml'
 
 module Unenviable
-  def check
-    return [[], [], []]
+  def self.check
+    load_env_descriptions unless @env_list
+    discrepancies = { required: [], optional: [], forbidden: [] }
+
+    @env_list.each do |var, details|
+      discrepancies[:required] << var if details[:required] && !ENV[var]
+      discrepancies[:optional] << var if !details[:required] && !details[:forbidden] && !ENV[var]
+      discrepancies[:forbidden] << var if details[:forbidden] && ENV[var]
+    end
+
+    discrepancies
+  end
+
+  def self.env_descriptions_file_location
+    'config/unenviable.yml'
+  end
+
+  def self.env_descriptions=(env_list)
+    @env_list = env_list
+  end
+
+  private
+
+  # Load the file that descriptions the required environment variables
+  # this needn't be called directly, it's called lazily by the functions.
+  def self.load_env_descriptions
+    if File.file?(env_descriptions_file_location)
+      @env_list = YAML.load(File.open(env_descriptions_file_location))
+    else
+      @env_list = {}
+    end
   end
 end
--- spec/unenviable.rb
+++ spec/unenviable.rb
@@ -1,16 +1,63 @@
 # encoding: utf-8
 require 'spec_helper'
 
+module Unenviable
+  def self.env_descriptions_file_location
+    'tmp/unenviable_test/unenviable.yml'
+  end
+end
+
 describe Unenviable do
   before :each do
+    Dir.mkdir File.join('', 'tmp', 'unenviable_test')
+    @config_dir = Dir.new File.join('', 'tmp', 'unenviable_test')
   end
 
   after :each do
+    @config_dir.each { |f| f.unlink unless f == '.' || f == '..' }
+    Dir.unlink @config_dir
   end
 
   describe '#check' do
     it 'passes silently when no variables are specified' do
-      expect(Unenviable.check).to eq([[], [], []])
+      expect(Unenviable.check).to eq(required: [], optional: [], forbidden: [])
     end
+
+    it 'passes silently when required variables are in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test', required: true } }
+      ENV['VALID_VAR'] = 'TRUE'
+      expect(Unenviable.check).to eq(required: [], optional: [], forbidden: [])
+    end
+
+    it 'returns missing list when required variables are not in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test', required: true } }
+      ENV.delete 'VALID_VAR'
+      expect(Unenviable.check).to eq(required: ['VALID_VAR'], optional: [], forbidden: [])
+    end
+
+    it 'passes silently when optional variables are in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test' } }
+      ENV['VALID_VAR'] = 'TRUE'
+      expect(Unenviable.check).to eq(required: [], optional: [], forbidden: [])
+    end
+
+    it 'returns optional list when optional variables are not in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test' } }
+      ENV.delete 'VALID_VAR'
+      expect(Unenviable.check).to eq(required: [], optional: ['VALID_VAR'], forbidden: [])
+    end
+
+    it 'passes silently when forbidden variables are not in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test', forbidden: true } }
+      ENV.delete 'VALID_VAR'
+      expect(Unenviable.check).to eq(required: [], optional: [], forbidden: [])
+    end
+
+    it 'returns forbidden list when forbidden variables are in the environment' do
+      Unenviable.env_descriptions= { 'VALID_VAR' => { description: 'test', forbidden: true } }
+      ENV['VALID_VAR'] = 'TRUE'
+      expect(Unenviable.check).to eq(required: [], optional: [], forbidden: ['VALID_VAR'])
+    end
+
   end
 end
